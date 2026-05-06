66d825809a56cfde3299dbfbe11ef75948dd08fe
RIYA <radhadas55515@gmail.com>
2026-05-07 01:22:27 +0530

    database fixed

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "vehicle_booking"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 db.connect((err) => {
