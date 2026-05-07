9b4b85f799e842c336b88947926ee8553ff60444
RIYA <radhadas55515@gmail.com>
2026-05-07 21:10:50 +0530

    final env fix

--- BACKEND/db.js
+++ BACKEND/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASS || "",
+    database: process.env.DB_NAME || "vehicle_booking",
+    port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
