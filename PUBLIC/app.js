ec99d329b7ffd92dca523e322ef5fc83854a3c6d
RIYA <radhadas55515@gmail.com>
2026-05-09 14:49:35 +0530

    remove localhost

--- PUBLIC/app.js
+++ PUBLIC/app.js
@@ -118,7 +118,7 @@ async function bookNow() {
     }
 
     try {
-        const response = await fetch("http://localhost:5000/bookings", {
+        const response = await fetch("/bookings", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
